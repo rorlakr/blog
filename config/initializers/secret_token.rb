09f952d41a68e19563ec173ba542ecfeefbb312a
Hyo Seong Choi <rorlab@gmail.com>
2013-10-28 15:55:08 +0900

    blog 어플리케이션 생성함

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'e6ae2ac9841b7b3089b18a11547f9d17f374fa992422f9e4de150a3bc212c22752907b04928d283342afbe31068f46f9ab462c0dedff86c59f52ba0e4a836642'
